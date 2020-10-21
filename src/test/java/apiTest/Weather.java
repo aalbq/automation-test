c6228b726f44d6efcdd527f96afab146539c8c32
Alexandra Albuquerque <alexandra.albuquerque@gmailcom>
2020-10-20 22:40:08 -0300

    Add initial skeleton files for the GetWeather API.

---
+++ src/test/java/apiTest/Weather.java
@@ -0,0 +1,24 @@
+package apiTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Weather {
+
+	@Before
+	public void start() {
+
+	}
+
+	@After
+	public void end() {
+
+	}
+
+	@Test
+	public void testFoo() {
+
+	}
+
+}
